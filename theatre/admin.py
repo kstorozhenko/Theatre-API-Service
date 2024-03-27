1f7b1d8fe6143ef9ed5fbbb455ebdb723ff1e073
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-27 13:14:22 +0200

    add models to admin panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
 
-from .models import TheatreHall
+from .models import (
+    TheatreHall,
+    Genre,
+    Actor,
+    Play,
+    Perfomance,
+    Reservation,
+    Ticket
+)
 
 
 admin.site.register(TheatreHall)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
+admin.site.register(Perfomance)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
