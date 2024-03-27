c927ec4c0f32e1cfcef94a520fde4b6f83a7dffd
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-27 16:04:13 +0200

    created views and serializers for Genre, Actor and Theatre hall

---
+++ theatre/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from theatre.models import Genre, Actor, TheatreHall, Play
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+
--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status, generics
 
-# Create your views here.
+from rest_framework.viewsets import GenericViewSet
+from theatre.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+from theatre.models import Genre, Actor, TheatreHall
+from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class TheatreHallViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
