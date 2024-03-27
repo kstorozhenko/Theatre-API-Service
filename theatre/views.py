6aa8bf2978c3c5c4cd54f1cdafe83809471731ad
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-27 16:54:18 +0200

    created views for Play and Perfomance

--- theatre/views.py
+++ theatre/views.py
@@ -1,10 +1,28 @@
+from datetime import datetime
+
 from rest_framework import viewsets, mixins, status, generics
+
+from django.db.models import F, Count
 
 from rest_framework.viewsets import GenericViewSet
 from theatre.permissions import IsAdminOrIfAuthenticatedReadOnly
 
-from theatre.models import Genre, Actor, TheatreHall
-from theatre.serializers import GenreSerializer, ActorSerializer, TheatreHallSerializer
+from theatre.models import (
+    Genre,
+    Actor,
+    TheatreHall,
+    Play,
+    Perfomance
+)
+from theatre.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    TheatreHallSerializer,
+    PlaySerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
+    PerfomanceSerializer, PerfomanceListSerializer, PerfomanceDetailSerializer
+)
 
 
 class GenreViewSet(
@@ -35,3 +53,89 @@ class TheatreHallViewSet(
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class PlayViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = Play.objects.prefetch_related("genres", "actors")
+    serializer_class = PlaySerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the movies with filters"""
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_ids = self._params_to_ints(genres)
+            queryset = queryset.filter(genres__id__in=genres_ids)
+
+        if actors:
+            actors_ids = self._params_to_ints(actors)
+            queryset = queryset.filter(actors__id__in=actors_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlayListSerializer
+
+        elif self.action == "retrieve":
+            return PlayDetailSerializer
+
+        else:
+            return PlaySerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Perfomance.objects.all()
+        .select_related("play", "theatre_hall")
+        .annotate(
+            tickets_available=(
+                    F("theatre_hall__rows") * F("theatre_hall__seats_in_row")
+                    - Count("tickets")
+            )
+        )
+    )
+    serializer_class = PerfomanceSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        movie_id_str = self.request.query_params.get("movie")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if movie_id_str:
+            queryset = queryset.filter(movie_id=int(movie_id_str))
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PerfomanceListSerializer
+
+        if self.action == "retrieve":
+            return PerfomanceDetailSerializer
+
+        return PerfomanceSerializer
