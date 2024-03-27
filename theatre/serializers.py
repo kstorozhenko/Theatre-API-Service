394a75803529387bb0abd4533522f1e146a9d0e8
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-27 16:54:04 +0200

    created serializers for Play and Perfomance

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from theatre.models import Genre, Actor, TheatreHall, Play
+from theatre.models import (
+    Genre,
+    Actor,
+    TheatreHall,
+    Play,
+    Perfomance,
+    Reservation,
+    Ticket
+)
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -21,4 +29,99 @@ class TheatreHallSerializer(serializers.ModelSerializer):
         fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
+class PlaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors"
+        )
 
+
+class PlayListSerializer(PlaySerializer):
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="full_name"
+    )
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+        )
+
+
+class PlayDetailSerializer(PlaySerializer):
+    actors = ActorSerializer(many=True, read_only=True)
+    genres = GenreSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Play
+        fields = (
+            "id",
+            "title",
+            "description",
+            "genres",
+            "actors",
+        )
+
+
+class PerfomanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Perfomance
+        fields = (
+            "id",
+            "play",
+            "show_time",
+            "theatre_hall"
+        )
+
+
+class PerfomanceListSerializer(PerfomanceSerializer):
+    play_title = serializers.CharField(
+        source="play.title", read_only=True
+    )
+    theatre_hall_name = serializers.CharField(
+        source="theatre_hall.name", read_only=True
+    )
+    theatre_hall_capacity = serializers.IntegerField(
+        source="theatre_hall.capacity", read_only=True
+    )
+    tickets_available = serializers.IntegerField(
+        read_only=True
+    )
+
+    class Meta:
+        model = Perfomance
+        fields = (
+            "id",
+            "play",
+            "show_time",
+            "theatre_hall",
+            "play_title",
+            "theatre_hall_name",
+            "theatre_hall_capacity",
+            "tickets_available"
+        )
+
+
+class PerfomanceDetailSerializer(PerfomanceSerializer):
+    play = PlayListSerializer(many=False, read_only=True)
+    theatre_hall = TheatreHallSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Perfomance
+        fields = ("id", "show_time", "play", "theatre_hall")
