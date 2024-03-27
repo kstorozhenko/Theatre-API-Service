0b4ed0e8aec66c19b8fa4fcbb5df6584aad407ce
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-27 16:55:17 +0200

    registered routers for plays and performances

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from theatre.views import GenreViewSet, ActorViewSet, TheatreHallViewSet
+from theatre.views import (
+    GenreViewSet,
+    ActorViewSet,
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet
+)
 
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
 router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
 
 
 urlpatterns = [path("", include(router.urls))]
